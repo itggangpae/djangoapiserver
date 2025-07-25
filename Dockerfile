922e01c528064be8ca24e85aebc58eedf69d78e0
ggangpae1 <ggangpae1@gmail.com>
2025-07-25 12:35:23 +0900

    데이터 수정

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,5 @@
 FROM python:3.8-slim-buster as build
 
-
-
 WORKDIR /usr/src/app
 
 COPY requirements.txt ./
--- apiserver/views.py
+++ apiserver/views.py
@@ -4,5 +4,5 @@ from rest_framework import status
 
 @api_view(['GET'])
 def index(request):
-    data = {"result":"success", "data":[{"id":"itstudy", "name":"군계"}]}
+    data = {"result":"success", "data":[{"id":"itstudy", "name":"아담"}]}
     return Response(data, status=status.HTTP_200_OK)
